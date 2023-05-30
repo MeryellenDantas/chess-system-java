0a7aa15cfc1f983268adc09e8bbb342fa5f1cb67
meryellendantas <meryellen.fdantas@gmail.com>
2023-05-30 18:09:20 +0100

    Project Created

---
+++ src/Application/Program.java
@@ -0,0 +1,16 @@
+package Application;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+
+    }
+}
