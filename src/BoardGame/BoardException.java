703273b6ab8ed9db3cc0f8088b6927c7de291923
meryellendantas <meryellen.fdantas@gmail.com>
2023-05-31 15:42:20 +0100

    BoardException and defensive programming

---
+++ src/BoardGame/BoardException.java
@@ -0,0 +1,10 @@
+package BoardGame;
+
+public class BoardException extends RuntimeException{
+
+    private static final long seriaVersionUID = 1L;
+
+    public BoardException(String msg){
+        super(msg);
+    }
+}
