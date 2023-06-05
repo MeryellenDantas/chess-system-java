919addccd29356959bc148de9eb5c53d1e15c33d
meryellendantas <meryellen.fdantas@gmail.com>
2023-06-05 14:52:10 +0100

    Fix - better treatment to Promotion

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -5,10 +5,8 @@ import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.ChessPosition;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.security.InvalidParameterException;
+import java.util.*;
 
 
 public class Program {
@@ -44,7 +42,11 @@ public class Program {
 
                 if (chessMatch.getPromoted() != null){
                     System.out.println("Enter piece for promotion (B/N/R/Q): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
+                        System.out.println("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
 
